2bab5e4b5f65a1b52ef95b81c1ed9951a9e24723
Dinesh Kumar <ashokdinesh89@gmail.com>
2022-06-29 02:44:03 -0400

    Initial Commit

---
+++ WordleProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordleProject
+//
+//  Created by Dinesh Kumar Raj Kumar on 2022-06-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordleProject/WordleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleProjectApp.swift
+//  WordleProject
+//
+//  Created by Dinesh Kumar Raj Kumar on 2022-06-29.
+//
+
+import SwiftUI
+
+@main
+struct WordleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
